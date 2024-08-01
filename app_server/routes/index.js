8bb1116172251385e0743ab50f5a1972120120c6
asishadimulapu <150647669+asishadimulapu@users.noreply.github.com>
2024-08-01 12:27:19 +0000

    ms

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const ctrlMain = require("../controllers/main");
+const express = require("express");
 const router = express.Router();
-const ctrlLocations =
-require('../controllers/locations');
-const ctrlOthers = require('../controllers/others');
+const ctrlLocations = require("../controllers/locations");
+const ctrlOthers = require("../controllers/others");
 /* Locations pages */
-router.get('/', ctrlLocations.homelist);
-router.get('/location', ctrlLocations.locationInfo);
-router.get('/location/review/new', ctrlLocations.addReview);
+router.get("/", ctrlLocations.homelist);
+router.get("/location", ctrlLocations.locationInfo);
+router.get("/location/review/new", ctrlLocations.addReview);
 /* Other pages */
-router.get('/about', ctrlOthers.about);
-module.exports = router
+router.get("/about", ctrlOthers.about);
+/* GET homepage. */
+router.get("/", ctrlMain.index);
+module.exports = router;
